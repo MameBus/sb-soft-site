437ad5ec239865538d32b1dd8f52acbb2182ac3e
Gary Stu <joel.tonkins@outlook.com>
2026-05-17 23:49:02 +1000

    Add quotes

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,7 +149,7 @@ resource "aws_cloudfront_distribution" "cdn" {
 # ---------- cloudflare ----------
 
 data "cloudflare_zone" "main" {
-  zone_id = d531a4ca95a31dacbe6fb4132f127a0e
+  zone_id = "d531a4ca95a31dacbe6fb4132f127a0e"
 }
 
 // https://registry.terraform.io/providers/-/aws/latest/docs/resources/acm_certificate_validation
