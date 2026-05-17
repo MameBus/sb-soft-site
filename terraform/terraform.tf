d49174c55784371ff4a554aabef600a73b96246c
Gary Stu <joel.tonkins@outlook.com>
2026-05-17 23:13:10 +1000

    Fix cloudflare provider

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.92"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
   }
 
   required_version = ">= 1.2"
