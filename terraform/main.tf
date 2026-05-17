e63674be14445b8c7c5de2d97f6eb6da1e4dd1bc
Gary Stu <joel.tonkins@outlook.com>
2026-05-18 09:03:59 +1000

    Update terraform to use s3 backend for state

--- terraform/main.tf
+++ terraform/main.tf
@@ -138,7 +138,6 @@ resource "aws_cloudfront_distribution" "cdn" {
     }
   }
 
-# Cert TODO change to custom domain name
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
     ssl_support_method  = "sni-only"
