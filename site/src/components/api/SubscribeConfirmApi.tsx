d261af0ff9ce8846a124f8ae5ec14390d85b3ffc
Gary Stu <joel.tonkins@outlook.com>
2026-06-01 14:25:02 +1000

    Move reading query parameters into React components

--- site/src/components/api/SubscribeConfirmApi.tsx
+++ site/src/components/api/SubscribeConfirmApi.tsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
 import RequestAndWait from "./RequestAndWait";
 
-type SubscribeConfirmApiProps = {
-    requestBody: any
+type ConfirmRequestParams = {
+    email: string,
+    token: string
 }
 
-function SubscribeConfirmApi({ requestBody } : SubscribeConfirmApiProps ) {
+function SubscribeConfirmApi() {
     const responseMapping = {
         "Already Verified": <p>This Email is already verified.</p>,
         "Success": <p>Success! You should receive a confirmation Email shortly.</p>,
         "Verification Failed": <p>Your token has expired. Please request a new verification Email from <a className='text-blue-500 underline' href='/unsubscribe'>here</a>.</p>
     }
 
+    const [body, setBody] = useState<ConfirmRequestParams | null>(null);
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search);
+
+        setBody({
+            email: queryParams.get('email') || '',
+            token: queryParams.get('token') || ''
+        });
+    }, []);
+
     return <div className="w-full">
-        <RequestAndWait endpoint="/subscribe-confirm" requestBody={requestBody} responseMapping={responseMapping} />
+        <RequestAndWait endpoint="/subscribe-confirm" requestBody={body} responseMapping={responseMapping} />
     </div>
 }
 
